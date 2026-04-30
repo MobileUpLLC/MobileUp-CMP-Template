MobileUpLLC/MobileUp-Android-Template

PermissionService

core/src/androidMain/kotlin/ru/mobileup/template/core/PlatformDI.kt
@@ -11,6 +11,8 @@ import ru.mobileup.template.core.configuration.BuildType
 import ru.mobileup.template.core.configuration.Configuration
 import ru.mobileup.template.core.network.createKtorLogger
 import ru.mobileup.template.core.network.createOkHttpEngine
+import ru.mobileup.template.core.permissions.PermissionService
+import ru.mobileup.template.core.permissions.PermissionServiceImpl
 import ru.mobileup.template.core.settings.AndroidSettingsFactory
 import ru.mobileup.template.core.settings.SettingsFactory
 
@@ -25,4 +27,5 @@ actual fun platformCoreModule(configuration: Configuration) = module {
     }
     single<NetworkConnectivityProvider> { AndroidNetworkConnectivityProvider(get()) }
     single<SettingsFactory> { AndroidSettingsFactory(get(), Dispatchers.IO) }
+    single<PermissionService>(createdAtStart = true) { PermissionServiceImpl(get(), get(), get()) }
 }

core/src/iosMain/kotlin/ru/mobileup/template/core/PlatformDI.kt
@@ -7,6 +7,8 @@ import ru.mobileup.template.core.configuration.BuildType
 import ru.mobileup.template.core.configuration.Configuration
 import ru.mobileup.template.core.network.createKtorLogger
 import ru.mobileup.template.core.network.createOkHttpEngine
+import ru.mobileup.template.core.permissions.PermissionService
+import ru.mobileup.template.core.permissions.PermissionServiceImpl
 import ru.mobileup.template.core.settings.IosSettingsFactory
 import ru.mobileup.template.core.settings.SettingsFactory
 
@@ -16,4 +18,5 @@ actual fun platformCoreModule(configuration: Configuration) = module {
         single { createKtorLogger() }
     }
     single<SettingsFactory> { IosSettingsFactory(Dispatchers.IO) }
+    single<PermissionService> { PermissionServiceImpl() }
 }

core/src/jvmMain/kotlin/ru/mobileup/template/core/PlatformDI.kt
@@ -5,10 +5,13 @@ import ru.mobileup.template.core.configuration.BuildType
 import ru.mobileup.template.core.configuration.Configuration
 import ru.mobileup.template.core.network.createKtorLogger
 import ru.mobileup.template.core.network.createOkHttpEngine
+import ru.mobileup.template.core.permissions.PermissionService
+import ru.mobileup.template.core.permissions.PermissionServiceImpl
 
 actual fun platformCoreModule(configuration: Configuration) = module {
     single { createOkHttpEngine() }
     if (configuration.buildType == BuildType.Debug) {
         single { createKtorLogger() }
     }
+    single<PermissionService> { PermissionServiceImpl() }
 }

MobileUpLLC/MobileUp-CMP-Template

core/src/androidMain/kotlin/ru/mobileup/template/core/PlatformDI.kt
@@ -11,6 +11,8 @@ import ru.mobileup.template.core.configuration.BuildType
 import ru.mobileup.template.core.configuration.Configuration
 import ru.mobileup.template.core.network.createKtorLogger
 import ru.mobileup.template.core.network.createOkHttpEngine
+import ru.mobileup.template.core.permissions.PermissionService
+import ru.mobileup.template.core.permissions.AndroidPermissionService
 import ru.mobileup.template.core.settings.AndroidSettingsFactory
 import ru.mobileup.template.core.settings.SettingsFactory
 
@@ -25,4 +27,5 @@ actual fun platformCoreModule(configuration: Configuration) = module {
     }
     single<NetworkConnectivityProvider> { AndroidNetworkConnectivityProvider(get()) }
     single<SettingsFactory> { AndroidSettingsFactory(get(), Dispatchers.IO) }
+    single<PermissionService>(createdAtStart = true) { AndroidPermissionService(get(), get(), get()) }
 }

core/src/iosMain/kotlin/ru/mobileup/template/core/PlatformDI.kt
@@ -7,6 +7,8 @@ import ru.mobileup.template.core.configuration.BuildType
 import ru.mobileup.template.core.configuration.Configuration
 import ru.mobileup.template.core.network.createKtorLogger
 import ru.mobileup.template.core.network.createOkHttpEngine
+import ru.mobileup.template.core.permissions.PermissionService
+import ru.mobileup.template.core.permissions.IosPermissionService
 import ru.mobileup.template.core.settings.IosSettingsFactory
 import ru.mobileup.template.core.settings.SettingsFactory
 
@@ -16,4 +18,5 @@ actual fun platformCoreModule(configuration: Configuration) = module {
         single { createKtorLogger() }
     }
     single<SettingsFactory> { IosSettingsFactory(Dispatchers.IO) }
+    single<PermissionService> { IosPermissionService() }
 }

core/src/jvmMain/kotlin/ru/mobileup/template/core/PlatformDI.kt
@@ -5,10 +5,13 @@ import ru.mobileup.template.core.configuration.BuildType
 import ru.mobileup.template.core.configuration.Configuration
 import ru.mobileup.template.core.network.createKtorLogger
 import ru.mobileup.template.core.network.createOkHttpEngine
+import ru.mobileup.template.core.permissions.PermissionService
+import ru.mobileup.template.core.permissions.JvmPermissionService
 
 actual fun platformCoreModule(configuration: Configuration) = module {
     single { createOkHttpEngine() }
     if (configuration.buildType == BuildType.Debug) {
         single { createKtorLogger() }
     }
+    single<PermissionService> { JvmPermissionService() }
 }