276429175e5ef48b9a2940b5ba3d2663e5b20fb8
a.artikov <artur.artikov@mobileup.ru>
2026-05-26 20:23:21 +0300

    WIP: Yandex Map for iOS

--- core/src/iosMain/kotlin/ru/mobileup/template/core/configuration/Platform.kt
+++ core/src/iosMain/kotlin/ru/mobileup/template/core/configuration/Platform.kt
@@ -1,5 +1,12 @@
 package ru.mobileup.template.core.configuration
 
-actual class Platform {
+import androidx.compose.runtime.staticCompositionLocalOf
+import ru.mobileup.template.core.map.internal.IosMapControllerFactory
+
+actual class Platform(
+    val iosMapControllerFactory: IosMapControllerFactory
+) {
     actual val type: PlatformType = PlatformType.Ios
 }
+
+val LocalPlatform = staticCompositionLocalOf<Platform> { error("Platform is not set") }
